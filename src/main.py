21a58b8128fddd8d3175f1d3fbf60a2293d6ae16
Sereth69 <damitnoob@hotmail.com>
2025-03-18 17:14:26 +1300

    updating for course changes in march 2025

--- src/main.py
+++ src/main.py
@@ -1,29 +1,26 @@
 import os
 import shutil
-from textnode import TextNode, TextType
-from copy_static import copy_directory
-from generate_page import generate_page  # Import the generate_page function
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    for root, dirs, files in os.walk(dir_path_content):
-        for file in files:
-            if file.endswith('.md'):
-                from_path = os.path.join(root, file)
-                relative_path = os.path.relpath(from_path, dir_path_content)
-                dest_path = os.path.join(dest_dir_path, os.path.splitext(relative_path)[0] + '.html')
-                os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-                generate_page(from_path, template_path, dest_path)
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    # Delete anything in the public directory
-    if os.path.exists('public'):
-        shutil.rmtree('public')
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    # Copy static files to public directory
-    copy_directory("static", "public")
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
-    # Generate pages recursively from content directory using template.html
-    generate_pages_recursive('content', 'template.html', 'public')
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
-if __name__ == "__main__":
-    main()
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,37 +1,44 @@
+from htmlnode import LeafNode
 from enum import Enum
 
+
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
-        if not isinstance(text_type, TextType):
-            raise ValueError("text_type must be an instance of TextType enum")
         self.text = text
-        self.text_type = text_type.value
+        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
-        if isinstance(other, TextNode):
-            return self.text == other.text and self.text_type == other.text_type and self.url == other.url
-        return False
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
     def __repr__(self):
-        return f"TextNode(text='{self.text}', text_type='{self.text_type}', url='{self.url}')"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
-# Example usage
-node1 = TextNode("Hello, world!", TextType.NORMAL)
-node2 = TextNode("Hello, world!", TextType.NORMAL)
-node3 = TextNode("Click here", TextType.LINK, "http://example.com")
 
-print(node1)  # Output: TextNode(text='Hello, world!', text_type='normal', url='None')
-print(node2)  # Output: TextNode(text='Hello, world!', text_type='normal', url='None')
-print(node3)  # Output: TextNode(text='Click here', text_type='link', url='http://example.com')
-
-print(node1 == node2)  # Output: True
-print(node1 == node3)  # Output: False
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
